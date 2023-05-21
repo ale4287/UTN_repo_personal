eda2fbba5e7be1e5834a59926940b571c2108274
ale4287 <alejandro.algarin@hotmail.com>
2023-05-21 09:14:00 -0300

    Leccion 05 JavaScript

--- Tecnicatura3Js/TSLeccion04/01-04-objetos.js
+++ Tecnicatura3Js/TSLeccion04/01-04-objetos.js
@@ -7,9 +7,20 @@ let persona = {
   apellido: 'Gil',
   email: 'cgil@gmail.com',
   edad: 30,
+  idioma: 'es',
+  get lang(){
+    return this.idioma.toUpperCase(); //Convierte las minusculas a mayusculas
+  },
+  set lang(lang){
+    this.idioma = lang.toUpperCase();
+  },
   nombreCompleto: function(){ //metodo o funcion en JavaScript
     return this.nombre+' '+this.apellido;
+  },
+  get nombreEdad(){ //Este es el metodo get
+    return 'El nombre es: '+this.nombre+', Edad: '+this.edad;
   }
+  
 }
 
 console.log(persona.nombre);
@@ -57,3 +68,88 @@ console.log(personaArray);
 console.log('Distintas formas de imprimir un objeto: forma 4');
 let personaString = JSON.stringify(persona);
 console.log(personaString);
+
+console.log('Comenzamos a utilizar el metodo get');
+console.log(persona.nombreEdad);
+
+console.log('Comenzamos con el metodo get y set para idiomas');
+persona.lang = 'en';
+console.log(persona.lang);
+
+function Persona3(nombre, apellido, email){ //Constructor
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.email = email;
+  this.nombreCompleto = function(){
+    return this.nombre+' '+this.apellido;
+  }
+}
+let padre = new Persona3('Leo', 'Lopez', 'lopezl@mail.com');
+padre.nombre = 'Luis'; // modificamos el nombre 
+padre.telefono = '549261828281'; //Una propiedad exclusiva del objeto padre
+console.log(padre);
+console.log(padre.nombreCompleto()); //Utilizamos la funcion
+let madre = new Persona3('Laura', 'Contrera', 'contreral@gmail.com');
+console.log(madre);
+console.log(madre.telefono); //la propiedad no esta definida
+console.log(madre.nombreCompleto());
+
+//Diferentes formas de crear objetos
+//caso numero 1
+let miObjeto = new Object(); //Esta es una opcion formal
+//caso numero 2
+let miObjeto2 = {}; //Esta opcion es breve y recomendada
+
+//caso String 1
+let miCadena1 = new String('Hola'); //Sintaxis formal
+//caso String 2
+let miCadena2 = 'Hola'; //Esta es la sintaxis simplificada y recomendada
+
+//caso con numeros 1
+let miNumero = new Number(1); //Es formal no recomendable
+//caso con numeros 2
+let miNumero2 = 1; //Sintaxis recomendada
+
+//caso boolean 1
+let miBoolena1 = new Boolean(false); //Formal
+//caso boolean 2
+let miBoolena2 = false; //Sintaxis recomendada
+
+//caso Arreglos 1
+let miArreglo1 = new Array(); //Formal
+//caso Arreglos 2
+let miArreglo2 = []; //Sintaxis recomendada
+
+//caso function 1
+let miFunction = new Function(); //Todo despues de new es considerado objeto
+//caso function 2
+let miFunction2 = function(){}; //Notacion simplificada y recomendada
+
+//Uso de prototype
+Persona3.prototype.telefono = '2618383343';
+console.log(padre);
+console.log(madre.telefono);
+madre.telefono = '43543563465';
+console.log(madre.telefono);
+
+//Uso de call
+let persona4 = {
+  nombre: 'Juan',
+  apellido: 'Perez',
+  nombreCompleto2: function(titulo, telefono){
+    return titulo+' '+this.nombre+' '+this.apellido+' '+telefono;
+    //return this.nombre+' '+this.apellido;
+  }
+}
+
+let persona5 = {
+  nombre: 'Carlos',
+  apellido: 'Lara'
+}
+
+console.log(persona4.nombreCompleto2('lic.', '345344576457'));
+console.log(persona4.nombreCompleto2.call(persona5, 'Ing.', '456454765685'));
+
+//Metodo Apply
+let arreglo = ['Ing.', '549087890809'];
+console.log(persona4.nombreCompleto2.apply(persona5, arreglo));
