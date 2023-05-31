a6927d9f0acf174bb90e961888794ca8dfbe7493
ale4287 <alejandro.algarin@hotmail.com>
2023-05-31 20:25:53 -0300

    Clase 07 JavaScript

--- Tecnicatura3Js/TSLeccion05/05-03-clases.js
+++ Tecnicatura3Js/TSLeccion05/05-03-clases.js
@@ -22,6 +22,16 @@ class Persona{ //Clase padre
     this._apellido = apellido;
   }
 
+  nombreCompleto(){
+    return this._nombre+' '+this._apellido;
+  }
+
+  //Sobreescribiendo el metodo de la clase padre (Object)
+  toString(){ //Regresa un String
+    //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
+    //El metodo que se ejecuta depende si es una referencia de tipo padre o hija
+    return  this.nombreCompleto();
+  }
 }
 
 class Empleado extends Persona{ //Clase hija
@@ -36,6 +46,15 @@ class Empleado extends Persona{ //Clase hija
 
   set departamento(departamento){
     this._departamento = departamento;
+  }
+
+  //Sobreescritura 
+  nombreCompleto(){
+    return super.nombreCompleto()+' '+this._departamento;
+  }
+
+  toString(){ //Regresa un String
+    return  this.nombreCompleto();
   }
 }
 
@@ -56,4 +75,8 @@ console.log(persona2.apellido);
 
 let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
+
+//Object.prototype.toString Esta es la manera de acceder a atributos y metodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
